4474d836d0c55a9c506a3bb23d75ff82f068cd5b
Younes Bouddou <ybouddou@e2r10p13.1337.ma>
2021-03-09 14:29:14 +0100

    splitting the input

--- builtins/ft_pwd.c
+++ builtins/ft_pwd.c
@@ -6,8 +6,13 @@
 /*   By: ybouddou <ybouddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 15:03:17 by ybouddou          #+#    #+#             */
-/*   Updated: 2021/03/07 15:03:19 by ybouddou         ###   ########.fr       */
+/*   Updated: 2021/03/09 10:51:40 by ybouddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	ft_pwd(t_mini *mini)
+{
+	(void)mini;
+}
--- pars_src/parsing.c
+++ pars_src/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cabouelw <cabouelw@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ybouddou <ybouddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 16:55:23 by cabouelw          #+#    #+#             */
-/*   Updated: 2021/03/08 17:06:26 by cabouelw         ###   ########.fr       */
+/*   Updated: 2021/03/09 14:16:31 by ybouddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,42 @@
 
 void	parse(t_mini *mini)
 {
+	int		i;
+	int		j;
+	char	**tmp;
+
+	i = 0;
+	j = 0;
+	tmp = NULL;
+	mini->input = ft_strtrim(mini->input, " \t");
 	ft_separate(mini);
 	// if (mini->check.value != '\0')
 		// printf("{%s}\n",mini->check.err);
+	mini->cmd = ft_split(mini->input, ';');
+	while (mini->cmd[i])
+	{
+		mini->cmd[i] = ft_strtrim(mini->cmd[i], " \t");
+		// tmp = ft_split(mini->cmd[i], '|');
+		// j = 0;
+		// while (tmp[j])
+		// {
+		// 	printf("\n---------------\n");
+		// 	mini->pip_list->content = tmp[j];
+		// 	if (tmp[j + 1])
+		// 		mini->pip_list->next = mini->pip_list;
+		// 	printf("{%s}\n", mini->pip_list->content);
+		// 	j++;
+		// }
+		// mini->pip_list->next = NULL;
+		printf("------\n(|%s|)\n", mini->cmd[i]);
+		i++;
+	}
+	// printf("\n---------------\n");
+	// while (mini->pip_list)
+	// {
+	// 	printf("{%s}\n", mini->pip_list->content);
+	// 	mini->pip_list = mini->pip_list->next;
+	// }
 	// mini->path_value = ft_lstsearch(mini->myenv, "PATH");
 	// mini->paths = ft_split(mini->path_value, ':');
 }
